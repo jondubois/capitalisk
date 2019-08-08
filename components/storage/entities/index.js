099b1fe1b0ae17c232831991ba56cdbda6360e99
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-08-09 00:11:42 +0200

    Extracted Lisk chain module into sidechain module

---
+++ components/storage/entities/index.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const Account = require('./account');
+const Block = require('./block');
+const Round = require('./round');
+const Transaction = require('./transaction');
+
+module.exports = {
+	Account,
+	Block,
+	Round,
+	Transaction,
+};
