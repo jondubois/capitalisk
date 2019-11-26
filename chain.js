b59b329ee762ba7e2685ba8fa7a34d88cd418a54
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-11-26 14:13:35 +0100

    Launch immediately

--- chain.js
+++ chain.js
@@ -168,14 +168,9 @@ module.exports = class Chain {
 			}
 			this._subscribeToEvents();
 
-			this.channel.subscribe('network:bootstrap', async () => {
-				this._calculateConsensus();
-				await this._startForging();
-			});
-
-			this.channel.subscribe('network:ready', async () => {
-				this._startLoader();
-			});
+      this._startLoader();
+      this._calculateConsensus();
+      await this._startForging();
 
 			// Avoid receiving blocks/transactions from the network during snapshotting process
 			if (!this.options.loading.rebuildUpToRound) {
